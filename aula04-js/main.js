5e3194c3f53ceb0d9a78c8d379d12b08fc79663e
Ivan Pires <ivanpires@gmail.com>
2026-09-21 19:34:06 -0400

    vetor como bd

--- aula04-js/main.js
+++ aula04-js/main.js
@@ -9,7 +9,16 @@ const btnSalvar = document.querySelector('#btnSalvar');
 // Adiciona um evento de clique ao botão de salvar
 btnSalvar.addEventListener('click', (e) => {
     e.preventDefault(); // evita que o formulário seja enviado por padrão.
-    console.log(inputNome.value);
-    console.log(inputEmail.value);
-    console.log(inputTelefone.value);
+    const cliente = {
+        id: Date.now(), // Gera um ID único baseado no timestamp atual
+        nome: inputNome.value,
+        email: inputEmail.value,
+        telefone: inputTelefone.value
+    }
+    clientes.push(cliente); // Adiciona o cliente ao vetor de clientes
+    document.querySelector('#formCliente').reset(); // Limpa o formulário após salvar
+    console.log(clientes); // Exibe o vetor de clientes no console para verificação
 });
+
+// Vetor de clientes (objeto cliente: {id, nome, email, telefone})
+const clientes = [];
